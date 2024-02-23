f6d1718ec26065ee7f709fcaeaebd2a186cf4d2a
sepsimus <kostya2120@yandex.ru>
2024-02-23 23:22:26 +0300

    add: Character highlightingAtackZone add

--- src/components/Character.js
+++ src/components/Character.js
@@ -3,12 +3,21 @@ import React, { useEffect, useState } from 'react';
 
 function Character() {
 
-    function choseEnemy(){
-
+    function highlightingAtackZone(event){
+        event.target.classList.toggle('character__atack-zone');
+        if(event.target.classList.contains('character__atack-zone')){
+            event.target.setAttribute('draggable', false);
+            return
+        }
+        event.target.setAttribute('draggable', true)
     }
 
     return (
-        <div className={`character`} draggable onClick={choseEnemy}/>
+        <>
+            <div className={`character`} draggable onClick={highlightingAtackZone}>
+                {/* <div className='character__atack-zone' /> */}
+            </div>
+        </>
     );
 }
 
