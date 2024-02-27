63cae5e17124d108858f55a2af2ca6f869b6ed1d
sepsimus <kostya2120@yandex.ru>
2024-02-28 01:09:08 +0300

    add: d6Dice generator and character attackAction(without defining a target) add

--- src/components/BattleMap.js
+++ src/components/BattleMap.js
@@ -2,14 +2,14 @@ import React, { useEffect, useState } from 'react';
 import Cell from './Cell';
 import Character from './Character';
 
-function BattleMap() {
+function BattleMap(props) {
 
     function renderCells(numberOfCells){
       let array = [];
       for(let i = 0; i < numberOfCells; i++){
         if(i === numberOfCells-1){
-            array.push(<Cell key={`cell_${i}`} isLast={true} />)  
-        }else array.push(<Cell key={`cell_${i}`}/>);
+            array.push(<Cell key={`cell_${i}`} isLast={true} throwD6Dice={props.throwD6Dice}/>)  
+        }else array.push(<Cell key={`cell_${i}`} throwD6Dice={props.throwD6Dice}/>);
     }
       return array;
     }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,21 +1,45 @@
 import { render } from '@testing-library/react';
 import React, { useEffect, useState } from 'react';
 
-function Character() {
+function Character(props) {
 
-
+    const [openedAttackZone, setOpenedAttackZone] = React.useState(false)
     const [characterHitPoints, setCharacterHitPoints] = React.useState(35);
 
-    function highlightingAttackZone(event){
-        const character = document.querySelector('.character');
-        const attackZone = document.querySelector('.character__attack-zone')
-
-        attackZone.classList.toggle('character__attack-zone_hidden');
-        if(!attackZone.classList.contains('character__attack-zone_hidden')){
-            character.setAttribute('draggable', false);
-            return
+    useEffect(()=>{
+        switch (true){
+            case characterHitPoints === 35: 
+                console.log('Здоров!')
+                console.log(`Здоровье: ${characterHitPoints}`);
+                break;
+            case characterHitPoints < 35 && characterHitPoints > 25:
+                console.log('Получил повреждения!')
+                console.log(`Здоровье: ${characterHitPoints}`);
+                break;
+            case characterHitPoints < 25 && characterHitPoints > 5:
+                console.log('Серьезные повреждения!')
+                console.log(`Здоровье: ${characterHitPoints}`);
+                break;
+            case characterHitPoints < 5 && characterHitPoints > 0:
+                console.log('Код красный!')
+                console.log(`Здоровье: ${characterHitPoints}`);
+                break;
+            case characterHitPoints <= 0:
+                console.log('Пульса нет...')
+                console.log(`Здоровье: ${characterHitPoints}`);
+                break;
         }
-        character.setAttribute('draggable', true)
+    },[characterHitPoints])
+
+    function highlightingAttackZone(event){
+        if(!openedAttackZone){
+            const character = document.querySelector('.character');
+            const attackZone = document.querySelector('.character__attack-zone')
+
+            attackZone.classList.remove('character__attack-zone_hidden');
+            character.setAttribute('draggable', false);
+            setOpenedAttackZone(true)
+        }
     }
 
     function showHitPoints(){
@@ -28,14 +52,24 @@ function Character() {
         document.querySelector('.character__tooltip').classList.add('character__tooltip_hidden')
     }
 
-    function attackAction(){
+    function attackAction(event, numberOfDice){
+        console.log('Атака!')
+        setCharacterHitPoints(characterHitPoints - props.throwD6Dice(numberOfDice));
         
+        const character = document.querySelector('.character');
+        const attackZone = document.querySelector('.character__attack-zone');
+        character.setAttribute('draggable', true);
+        attackZone.classList.add('character__attack-zone_hidden');
+        setOpenedAttackZone(false)
     }
 
     return (
         <>
             <div className={`character`} draggable onClick={highlightingAttackZone} onMouseEnter={showHitPoints} onMouseLeave={hideHitPoints}>
-                <div className='character__attack-zone character__attack-zone_hidden' draggable={false} onClick={attackAction}/>
+                <div className='character__attack-zone character__attack-zone_hidden' draggable={false} onClick={(e) => {
+                    attackAction(e, 4)
+                    }
+                }/>
                 <p className="character__tooltip character__tooltip_hidden">ПЗ: {characterHitPoints}</p>
             </div>
         </>
