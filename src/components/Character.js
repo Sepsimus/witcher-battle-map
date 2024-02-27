28dbdf723f12fd870a648b07a8a3a3c90926d760
sepsimus <kostya2120@yandex.ru>
2024-02-27 18:07:56 +0300

    add: show and hide HitPoints add

--- src/components/Character.js
+++ src/components/Character.js
@@ -3,19 +3,33 @@ import React, { useEffect, useState } from 'react';
 
 function Character() {
 
+    const character = document.querySelector('.character');
+
+    const [characterHitPoints, setCharacterHitPoints] = React.useState(35);
+
     function highlightingAtackZone(event){
-        event.target.classList.toggle('character__atack-zone');
-        if(event.target.classList.contains('character__atack-zone')){
+        event.target.classList.toggle('character_atack-zone');
+        if(event.target.classList.contains('character_atack-zone')){
             event.target.setAttribute('draggable', false);
             return
         }
         event.target.setAttribute('draggable', true)
     }
 
+    function showHitPoints(){
+        setTimeout(() => {
+            document.querySelector('.character__tooltip').classList.remove('character__tooltip_hidden')
+        }, 300)
+    }
+
+    function hideHitPoints(){
+        document.querySelector('.character__tooltip').classList.add('character__tooltip_hidden')
+    }
+
     return (
         <>
-            <div className={`character`} draggable onClick={highlightingAtackZone}>
-                {/* <div className='character__atack-zone' /> */}
+            <div className={`character`} draggable onClick={highlightingAtackZone} onMouseEnter={showHitPoints} onMouseLeave={hideHitPoints}>
+                <p className="character__tooltip character__tooltip_hidden">ПЗ: {characterHitPoints}</p>
             </div>
         </>
     );
