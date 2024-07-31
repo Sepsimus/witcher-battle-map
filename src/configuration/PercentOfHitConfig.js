6f147ae89d2c3b34d4525a7865013c83b914d033
sepsimus <kostya2120@yandex.ru>
2024-07-31 13:35:18 +0300

    add: PercentOfHitConfig

---
+++ src/configuration/PercentOfHitConfig.js
@@ -0,0 +1,69 @@
+const PercentOfHitConfig = {
+    '6': {
+        miss: '14%',
+        hit: '40%',
+        crit: '46%'
+    },
+    '5': {
+        miss: '19%',
+        hit: '44%',
+        crit: '37%'
+    },
+    '4': {
+        miss: '24%',
+        hit: '46%',
+        crit: '30%'
+    },
+    '3': {
+        miss: '30%',
+        hit: '46%',
+        crit: '24%'
+    },
+    '2': {
+        miss: '37%',
+        hit: '44%',
+        crit: '19%'
+    },
+    '1': {
+        miss: '46%',
+        hit: '40%',
+        crit: '14%'
+    },
+    '0': {
+        miss: '55%',
+        hit: '35%',
+        crit: '10%'
+    },
+    '-1': {
+        miss: '63%',
+        hit: '29%',
+        crit: '8%'
+    },
+    '-2': {
+        miss: '70%',
+        hit: '24%',
+        crit: '6%'
+    },
+    '-3': {
+        miss: '76%',
+        hit: '19%',
+        crit: '5%'
+    },
+    '-4': {
+        miss: '82%',
+        hit: '14%',
+        crit: '4%'
+    },
+    '-5': {
+        miss: '86%',
+        hit: '10%',
+        crit: '4%'
+    },
+    '-6': {
+        miss: '89%',
+        hit: '8%',
+        crit: '3%'
+    },
+}
+
+export default PercentOfHitConfig
